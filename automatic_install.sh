fde341b3c838afaa42e2b08ce076a3f0ce794e5f
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2015-11-30 10:16:56 +0700

    both dreamland and voyager scripts merged

--- automatic_install.sh
+++ automatic_install.sh
@@ -1,10 +1,64 @@
+#option configuration
+PS3='Which machine do you wish to install : '
+options=("dreamland" "voyager")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "dreamland")
+            machine="dreamland"
+	    break
+            ;;
+        "voyager")
+            machine="voyager"
+	    break
+            ;;
+        *) echo invalid option;;
+    esac
+done
+
+PS3='Which environment is it :'
+options=("test" "real")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "test")
+            environment="test"
+            break
+            ;;
+        "real")
+            environment="real"
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
+
+echo $machine
+echo $environment
+
 #sort mirrors by speed
-sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-sudo sh -c 'rankmirrors -n 5 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist'
-sudo pacman -Syu
+if [ $environment == "real" ]
+    then
+        echo 'Sorting mirrors by speed'
+        sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+        sudo sh -c 'rankmirrors -n 5 /etc/pacman.d/mirrorlist.bak > /etc/pacman.d/mirrorlist'
+        sudo pacman -Syu
+fi
 
 #graphic drivers
-sudo pacman -S --noconfirm xf86-video-vesa xf86-video-intel xf86-video-ati xf86-video-nouveau
+if [ $environment == "test" ]
+    then
+        echo 'Installing virtualbox graphic driver'
+        sudo pacman -S --noconfirm virtualbox-guest-utils
+elif [ $machine == "voyager" ]
+    then
+        echo 'Installing generic graphic drivers'
+        sudo pacman -S --noconfirm xf86-video-vesa xf86-video-intel xf86-video-ati xf86-video-nouveau
+elif [ $machine == "dreamland" ]
+    then
+        echo 'Installing nvidia driver'
+        sudo pacman -S --noconfirm nvidia
+fi
 
 #Xserver
 sudo pacman -S --noconfirm xorg-server xorg-server-utils xorg-apps
@@ -20,6 +74,7 @@ git config --global push.default simple
 sed -i '1s/^/eval $(ssh-agent)\n/' ~/.xinitrc
 
 #pacaur install
+sudo pacman -S --noconfirm expac
 mkdir /tmp/aur
 cd /tmp/aur
 git clone http://aur.archlinux.org/cower.git
@@ -32,6 +87,8 @@ cd ../pacaur
 makepkg -s
 sudo pacman -U --noconfirm pacaur*.pkg.tar.xz
 cd
+mkdir -p ~/.config/pacaur
+echo "displaybuildfiles=none" > ~/.config/pacaur/config
 
 #various applications for i3+firefox
 pacaur -S --noconfirm rxvt-unicode
@@ -50,20 +107,20 @@ git clone https://github.com/anhtuann/useful-scripts.git
 
 #vim
 pacaur -S --noconfirm vim
-ln -sf ~/Projects/dotfiles/voyager/vim/vimrc ~/.vimrc
+ln -sf ~/Projects/dotfiles/$machine/vim/vimrc ~/.vimrc
 
 #Xresources and bashrc added
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
-ln -sf ~/Projects/dotfiles/voyager/bash/bashrc ~/.bashrc
-ln -sf ~/Projects/dotfiles/voyager/Xresources/Xresources ~/.Xresources
+ln -sf ~/Projects/dotfiles/$machine/bash/bashrc ~/.bashrc
+ln -sf ~/Projects/dotfiles/$machine/Xresources/Xresources ~/.Xresources
 source ~/.bashrc
 
 #i3 config added
 mkdir ~/.i3
-ln -sf ~/Projects/dotfiles/voyager/i3/i3_config ~/.i3/config
+ln -sf ~/Projects/dotfiles/$machine/i3/i3_config ~/.i3/config
 pacaur -S --noconfirm rofi scrot playerctl feh volumeicon dunst
 mkdir -p ~/.config/i3status
-ln -sf ~/Projects/dotfiles/voyager/i3/i3status_config ~/.config/i3status/config
+ln -sf ~/Projects/dotfiles/$machine/i3/i3status_config ~/.config/i3status/config
 
 #various utilities
 pacaur -S --noconfirm alsa-utils
@@ -79,7 +136,7 @@ ssh-keygen
 echo 'eval $(ssh-agent)' >> ~/.xinitrc
 
 #vimperator
-ln -sf ~/Projects/dotfiles/voyager/vimperator/vimperatorrc ~/.vimperatorrc
+ln -sf ~/Projects/dotfiles/$machine/vimperator/vimperatorrc ~/.vimperatorrc
 
 #mtp support
 pacaur -S --noconfirm libmtp android-file-transfer
