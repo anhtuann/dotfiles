d1b517d354123b9a888c3c028c4f5859d630ae09
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2015-12-16 12:16:29 +0700

    small improvements after first test install

--- arch_install/install.sh
+++ arch_install/install.sh
@@ -120,7 +120,7 @@ pacaur -S --noconfirm ranger
 pacaur -S --noconfirm calibre blender
 
 #virtualbox
-pacaur -S --noconfirm virtualbox linux-headers
+pacaur -S --noconfirm virtualbox virtualbox-host-dkms linux-headers
 sudo dkms install vboxhost/$(pacman -Q virtualbox|awk '{print $2}'|sed 's/\-.\+//') -k $(uname -rm|sed 's/\ /\//')
 sudo gpasswd -a $USER vboxusers
 pacaur -S --noconfirm virtualbox-guest-iso net-tools
