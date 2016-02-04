f51bc7cf92aeac6b779970870ab402c5c8b25fb0
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-04 11:26:46 +0700

    zsh conf file in unique directory now

--- dreamland/other_installs/zsh.sh
+++ dreamland/other_installs/zsh.sh
@@ -1,4 +1,4 @@
 pacaur -S --noconfirm zsh
 chsh -s /bin/zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -sf ~/Projects/dotfiles/dreamland/zshrc_conf ~/.zshrc
+ln -sf ~/Projects/dotfiles/dreamland/confs/zshrc_conf ~/.zshrc
