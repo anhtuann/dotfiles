6ce49ec8822e706cb4dc13e184775a78f2af9fae
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-10 09:59:38 +0700

    added some lib32 dependencies for playonlinux

--- dreamland/other_installs/playonlinux.sh
+++ dreamland/other_installs/playonlinux.sh
@@ -1,2 +1,5 @@
 pacaur -S --noconfirm lib32-libldap
+pacaur -S --noconfirm lib32-virtualgl
+pacaur -S --noconfirm lib32-nvidia-utils
+pacaur -S --noconfirm lib32-primus
 pacaur -S --noconfirm playonlinux
