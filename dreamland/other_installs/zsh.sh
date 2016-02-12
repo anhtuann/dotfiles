2a2574f3a7ecf01b0bca20fdc83f7c6ff1d40b3c
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-12 10:51:05 +0700

    ohmyzsh changes shell by itself, no need to call the chsh command

--- dreamland/other_installs/zsh.sh
+++ dreamland/other_installs/zsh.sh
@@ -1,4 +1,3 @@
 pacaur -S --noconfirm zsh
-chsh -s /bin/zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 ln -sf ~/Projects/dotfiles/dreamland/confs/zshrc_conf ~/.zshrc
