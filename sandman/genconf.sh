b6d1047948cb0c423ccb2238afd7988967f96200
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-15 14:27:26 +0800

    zsh script correctly added to sandman

--- sandman/genconf.sh
+++ sandman/genconf.sh
@@ -1,5 +1,3 @@
-ln -sf ~/Projects/dotfiles/sandman/confs/zshrc_conf ~/.zshrc
-
 #vim
 ln -sf ~/Projects/dotfiles/sandman/confs/vimrc_conf ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
