73c0d1d1d1e14f653b2730d66bc24641713e126e
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-15 14:03:07 +0800

    phpini_conf added for wordpress setup

--- sandman/other_installs/wordpress.sh
+++ sandman/other_installs/wordpress.sh
@@ -1,3 +1,4 @@
 pacaur -S --noconfirm php php-fpm
 sudo systemctl enable php-fpm.service
 sudo systemctl start php-fpm.service
+sudo ln -sf ~/Projects/dotfiles/sandman/confs/phpini_conf /etc/php/php.ini
