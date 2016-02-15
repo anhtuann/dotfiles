443bc501cb8c51902c57412f3652804796355405
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-15 15:52:29 +0800

    zsh conf changed from dreamland to sandman

--- sandman/other_installs/zsh.sh
+++ sandman/other_installs/zsh.sh
@@ -1,3 +1,3 @@
 pacaur -S --noconfirm zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-ln -sf ~/Projects/dotfiles/dreamland/confs/zshrc_conf ~/.zshrc
+ln -sf ~/Projects/dotfiles/sandman/confs/zshrc_conf ~/.zshrc
