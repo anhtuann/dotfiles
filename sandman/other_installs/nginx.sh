7ed984103abea3f5a03a8ed01f050cc4b8e11c52
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-15 13:54:41 +0800

    nginx conf added to sandman

--- sandman/other_installs/nginx.sh
+++ sandman/other_installs/nginx.sh
@@ -1,3 +1,7 @@
 pacaur -S --noconfirm nginx
 sudo systemctl enable nginx.service
 sudo systemctl start nginx.service
+sudo mkdir /etc/nginx/servers-available
+sudo mkdir /etc/nginx/servers-enabled
+sudo ln -sf ~/Projects/dotfiles/sandman/confs/nginx_conf /etc/nginx/nginx.conf
+sudo systemctl restart nginx.service
