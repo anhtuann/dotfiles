ab1edbe605ca685b26736c975a27b99663de4871
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-16 18:01:37 +0700

    genconf modified with good path in sandman

--- machines/sandman/genconf.sh
+++ machines/sandman/genconf.sh
@@ -1,6 +1,6 @@
 #vim
-ln -sf ~/Projects/dotfiles/sandman/confs/vimrc_conf ~/.vimrc
+ln -sf ~/Projects/dotfiles/machines/sandman/confs/vimrc_conf ~/.vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-ln -sf ~/Projects/dotfiles/sandman/confs/tmux_conf ~/.tmux.conf
+ln -sf ~/Projects/dotfiles/machines/sandman/confs/tmux_conf ~/.tmux.conf
