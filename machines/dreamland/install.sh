e6f98194b4f76b0511c96099c218c6624fc4207f
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-02-16 21:44:18 +0700

    apparently whitespaces are very important in comparison statements

--- machines/dreamland/install.sh
+++ machines/dreamland/install.sh
@@ -18,8 +18,7 @@ env=$env ./genconf.sh
 cd ~/Projects/dotfiles/scripts/arch
 machine=$machine ./zsh.sh
 
-echo $env
-if [ $env=="real" ]; then
+if [ $env == "real" ]; then
     cd ~/Projects/dotfiles/scripts/arch/laptop
     ./android.sh
     ./bluetooth.sh
