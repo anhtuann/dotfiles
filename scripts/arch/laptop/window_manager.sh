cde40df4d14d4e6779e28f7722c2a848ea7fa778
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-06-21 11:55:19 +0700

    xcompmgr added to dreamland to fix firefox 47 lags

--- scripts/arch/laptop/window_manager.sh
+++ scripts/arch/laptop/window_manager.sh
@@ -4,6 +4,7 @@ sudo pacman -S --noconfirm i3
 #various applications for i3
 pacaur -S --noconfirm rxvt-unicode
 pacaur -S --noconfirm rofi scrot feh imagemagick volumeicon dunst
+pacaur -S --noconfirm xcompmgr
 
 #useful scripts downloaded from github
 git clone https://github.com/anhtuann/useful-scripts.git
