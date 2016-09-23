647bffdfcb987610903692b54f05f432e2a5d5cc
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-09-23 15:35:17 +0700

    deleted laptop script folder

--- scripts/arch/laptop/playonlinux.sh
+++
@@ -1,5 +0,0 @@
-pacaur -S --noconfirm lib32-libldap
-pacaur -S --noconfirm lib32-virtualgl
-pacaur -S --noconfirm lib32-nvidia-utils
-pacaur -S --noconfirm lib32-primus
-pacaur -S --noconfirm playonlinux
