69326a7be34c0092e8a3a087321e6794c73b7bd3
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2016-09-23 15:27:13 +0700

    ranger added as a cli tool

--- scripts/arch/cli_tools.sh
+++ scripts/arch/cli_tools.sh
@@ -2,6 +2,7 @@ pacaur -S --noconfirm vim
 pacaur -S --noconfirm tmux
 pacaur -S --noconfirm unrar unzip
 pacaur -S --noconfirm rsync
+pacaur -S --noconfirm ranger
 
 if [ $machine == "sandman" ]; then
     pacaur -S --noconfirm rxvt-unicode-terminfo
