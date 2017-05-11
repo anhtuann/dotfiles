127aaa4c013c14ea5f15461218784fe455956736
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-05-11 16:16:17 +0200

    libinput script relocated from useful_scripts

---
+++ scripts/arch/desktop_environment/libinput.sh
@@ -0,0 +1,9 @@
+#libinput-list-devices
+#xinput list-props device-number
+#xinput set-prop device-number option-number setting
+
+#Tap to click
+xinput set-prop 12 273 1 
+
+#Acceleration
+xinput set-prop 12 277 1 
