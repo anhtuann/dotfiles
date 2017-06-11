8244d9372a6c0da67ae91d03a6ff06baeb6f3f96
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 19:11:48 +0200

    cli_tools install script added, isolated vim in another script I need to write

---
+++ scripts/cli_tools.py
@@ -0,0 +1,12 @@
+import tools
+
+#INSTALLATION
+packages = ['unrar',
+            'unzip',
+            'rsync',
+            'ranger',]
+tools.pacaur(packages)
+
+#CONFIGURATION
+tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rifle_conf', '~/.config/ranger/rifle.conf')
+tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rc_conf', '~/.config/ranger/rc.conf')
