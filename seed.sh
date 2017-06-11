e8fb4a3dda161ee16769f2f0627199cbcd0c320c
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 15:44:18 +0200

    pacman mirrorlist optimization added to seed.sh

--- seed.sh
+++ seed.sh
@@ -1,4 +1,13 @@
-sudo pacman -Syu
+#Update and optimize mirrorlist for pacman
+#Code taken from a linode stackscript and added sudo to it
+#https://www.linode.com/stackscripts/view/12580
+sudo pacman -Sy --noconfirm wget reflector
+sudo mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.old
+sudo wget https://www.archlinux.org/mirrorlist/all/ -O /etc/pacman.d/mirrorlist
+sudo reflector --protocol http --sort rate --fastest 6 --threads 10 --save /etc/pacman.d/mirrorlist
+sudo pacman -Syu --noconfirm
+
+#Get dotfiles project
 sudo pacman -S git openssh
 mkdir ~/Projects
 cd ~/Projects
