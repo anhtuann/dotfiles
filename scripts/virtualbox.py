f138e379cb755594c551018008ae08a64e77e5bc
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 20:27:05 +0200

    virtualbox script added

---
+++ scripts/virtualbox.py
@@ -0,0 +1,16 @@
+import choices
+import tools
+
+#INSTALLATION
+if choices.INSTALL_ENV == 'virtualbox':
+    tools.pacaur(['virtualbox-guest-modules-arch'])
+else:
+    tools.pacaur(['virtualbox', 'virtualbox-host-dkms', 'linux-headers'])
+    command = "sudo dkms install vboxhost/$(pacman -Q virtualbox|awk '{print $2}'|sed 's/\-.\+//') -k $(uname -rm|sed 's/\ /\//')"
+    tools.bash_cmd(command.split(' '), shell=True)
+    command = "sudo gpasswd -a $USER vboxusers"
+    tools.bash_cmd(command.split(' '), shell=True)
+    tools.pacaur(['virtualbox-guest-iso',
+                    'net-tools',
+                    'virtualbox-ext-oracle',
+                    'qt4'])
