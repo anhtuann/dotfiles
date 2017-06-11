3954b0bdbcf486c21101ad7f1a68d57aaa2c0e8a
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 19:50:42 +0200

    ranger confs files replaced by my custom ones

--- scripts/cli_tools.py
+++ scripts/cli_tools.py
@@ -9,5 +9,8 @@ tools.pacaur(packages)
 
 #CONFIGURATION
 tools.bash_cmd(['ranger', '--copy-config=all'])
+with tools.cd('.config/ranger/'):
+    tools.bash_cmd(['rm', 'rifle.conf'])
+    tools.bash_cmd(['rm', 'rc.conf'])
 tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rifle_conf', '~/.config/ranger/rifle.conf')
 tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rc_conf', '~/.config/ranger/rc.conf')
