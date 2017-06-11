da001ce6f11724dfc7060236360c6afae5c327a0
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 20:52:29 +0200

    bumblebee added

---
+++ scripts/bumblebee.py
@@ -0,0 +1,19 @@
+import tools
+import choices
+
+#INSTALLATION
+if choices.INSTALL_ENV != 'virtualbox':
+    packages = ['bumblebee',
+                'mesa',
+                'xf86-video-intel',
+                'nvidia']
+    tools.pacaur(packages)
+    command = 'sudo gpasswd -a $USER bumblebee'
+    tools.bash_cmd(command.split(' '), shell=True)
+    command = 'sudo systemctl enable bumblebeed.service'    tools.bash_cmd(command.split(' '), shell=True)
+    tools.pacaur(['bbswitch-dkms'])
+
+#CONFIGURATION
+if choices.INSTALL_ENV != 'virtualbox':
+    tools.link_conf('~/Projects/dotfiles/confs/bumblebee_conf', '/etc/bumblebee/bumblebee.conf', sudo=True)
+    tools.link_conf('~/Projects/dotfiles/confs/bumblebee_nvidia_conf', '/etc/bumblebee/xorg.conf.nvidia', sudo=True)
