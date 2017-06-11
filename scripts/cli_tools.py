4f5638bcf355f7916c67fb64d7322c22e7446ca5
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 19:16:21 +0200

    ranger config folder created

--- scripts/cli_tools.py
+++ scripts/cli_tools.py
@@ -8,5 +8,6 @@ packages = ['unrar',
 tools.pacaur(packages)
 
 #CONFIGURATION
+tools.bash_cmd(['mkdir', '-p', '.config/ranger'])
 tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rifle_conf', '~/.config/ranger/rifle.conf')
 tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rc_conf', '~/.config/ranger/rc.conf')
