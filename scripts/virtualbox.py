dfc91e9022cf180be82b9c36459fd033fdde0cbf
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 22:55:34 +0200

    virtualbox on a testing machine improved

--- scripts/virtualbox.py
+++ scripts/virtualbox.py
@@ -3,18 +3,25 @@ import tools
 
 #INSTALLATION
 if choices.INSTALL_ENV == 'virtualbox':
-    tools.pacaur(['virtualbox-guest-modules-arch'])
+    packages = ['virtualbox-guest-modules-arch']
+    tools.pacaur(packages)
 else:
-    tools.pacaur(['virtualbox', 'virtualbox-host-dkms', 'linux-headers'])
+    packages = ['virtualbox', 
+                'virtualbox-host-dkms', 
+                'linux-headers',
+                'virtualbox-guest-iso',
+                'net-tools',
+                'virtualbox-ext-oracle',
+                'qt4']
+    tools.pacaur(packages)
     command = "sudo dkms install vboxhost/$(pacman -Q virtualbox|awk '{print $2}'|sed 's/\-.\+//') -k $(uname -rm|sed 's/\ /\//')"
     tools.bash_cmd(command.split(' '), shell=True)
     command = "sudo gpasswd -a $USER vboxusers"
     tools.bash_cmd(command.split(' '), shell=True)
-    tools.pacaur(['virtualbox-guest-iso',
-                    'net-tools',
-                    'virtualbox-ext-oracle',
-                    'qt4'])
     
 #CONFIGURATION
-if choices.INSTALL_ENV != 'virtualbox':
+if choices.INSTALL_ENV == 'virtualbox':
+    command = 'sudo systemctl enable vboxservice.service'
+    tools.bash_cmd(command.split(' '))
+else:
     tools.link_conf('~/Projects/dotfiles/confs/virtualbox_conf', '/etc/modules-load.d/virtualbox.conf', sudo=True)
