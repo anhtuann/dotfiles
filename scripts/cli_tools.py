eadfd302d6e90c9e9eea2eeeddfc805e453d908a
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 19:44:51 +0200

    ranger config command added

--- scripts/cli_tools.py
+++ scripts/cli_tools.py
@@ -8,6 +8,6 @@ packages = ['unrar',
 tools.pacaur(packages)
 
 #CONFIGURATION
-tools.mkdir('.config/ranger')
+tools.bash_cmd(['ranger', '--copy-config=all'])
 tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rifle_conf', '~/.config/ranger/rifle.conf')
 tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rc_conf', '~/.config/ranger/rc.conf')
