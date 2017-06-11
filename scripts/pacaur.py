004c418fc35baeaaa85df5c6db50299cbe54bbd9
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-11 17:16:22 +0200

    first draft of pacaur script with python based tools

---
+++ scripts/pacaur.py
@@ -0,0 +1,22 @@
+import tools
+
+packages = ['expac', 'yajl']
+tools.pacman(packages, noconfirm=False)
+
+tmp_folder = '/tmp/aur'
+tools.bash_cmd(['mkdir', tmp_folder])
+
+with tools.cd(tmp_folder):
+    tools.git_clone('https://aur.archlinux.org/cower.git')
+    tools.git_clone('https://aur.archlinux.org/pacaur.git')
+    tools.bash_cmd(['gpg', '--keyserver', 'pgp.mit.edu', '--recv-keys', 'F56C0C53'])
+    with tools.cd('cower'):
+        package = 'cower'+'*.pkg.tar.xz'
+        tools.makepkg(package)
+    with tools.cd('pacaur'):
+        package = 'pacaur'+'*.pkg.tar.xz'
+        tools.makepkg(package)
+
+tools.bash_cmd(['mkdir', '-p', '~/.config/pacaur'])
+tools.bash_cmd(['echo', '"displaybuildfiles=none"', '>' ,'~/.config/pacaur/config']
+
---
+++ scripts/tools.py
@@ -0,0 +1,37 @@
+import subprocess
+import contextlib
+import os
+
+def pacman(packages, makepkg=False, noconfirm=True):
+    command = ['sudo', 'pacman']
+    if not makepkg:
+        command.append('-S')
+    else:
+        command.append('-U')
+    if noconfirm:
+        command.append('--noconfirm')
+    for package in packages:
+        command.append(package)
+    subprocess.run(args = command, stdout=subprocess.PIPE)
+
+def bash_cmd(args):
+    subprocess.run(args, stdout=subprocess.PIPE)
+
+@contextlib.contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
+
+def git_clone(repository):
+    command = ['git', 'clone']
+    command.append(repository)
+    subprocess.run(args = command, stdout=subprocess.PIPE)
+
+def makepkg(package):
+    bash_cmd(['makepkg', '-s'])
+    pacman(list(package), makepkg=True)
+
