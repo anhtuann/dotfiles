8e6a87efb33a7272fd9ba4f80e8f2d43bb919369
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-12 11:13:51 +0200

    symlinks fixed in both cli_tools and vim scripts

--- scripts/cli_tools.py
+++ scripts/cli_tools.py
@@ -12,5 +12,5 @@ tools.bash_cmd(['ranger', '--copy-config=all'])
 with tools.cd('.config/ranger/'):
     tools.bash_cmd(['rm', 'rifle.conf'])
     tools.bash_cmd(['rm', 'rc.conf'])
-tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rifle_conf', '~/.config/ranger/rifle.conf')
-tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/ranger_rc_conf', '~/.config/ranger/rc.conf')
+tools.link_conf('~/Projects/dotfiles/confs/ranger_rifle_conf', '~/.config/ranger/rifle.conf')
+tools.link_conf('~/Projects/dotfiles/confs/ranger_rc_conf', '~/.config/ranger/rc.conf')
--- scripts/vim.py
+++ scripts/vim.py
@@ -10,4 +10,4 @@ tools.mkdir('.vim/bundle')
 with tools.cd('.vim/bundle'):
     tools.git_clone('https://github.com/VundleVim/Vundle.vim.git', 'Vundle.vim')
     tools.bash_cmd(['vim', '+PluginInstall', '+qall'])
-tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/vimrc_conf', '~/.vimrc')
+tools.link_conf('~/Projects/dotfiles/confs/vimrc_conf', '~/.vimrc')
