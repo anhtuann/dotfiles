ec00ab10cab15d532a3098377d5cd562fc8d5d5c
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-12 16:13:09 +0200

    little fix in post_reboot

--- scripts/shell.py
+++ scripts/shell.py
@@ -11,8 +11,3 @@ with tools.cd('~/.config'):
 command = 'sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"'
 tools.bash_cmd(command, shell=True)
 tools.link_conf('~/Projects/dotfiles/confs/zshrc_conf', '~/.zshrc')
-
-#may need to execute those two commands after reboot because of the change of shell
-#another solution would be to use Popen which has an executable argument
-tools.bash_cmd('source ~/.zshrc', shell=True)
-tools.bash_cmd('base16_3024', shell=True)
--- scripts/vim.py
+++ scripts/vim.py
@@ -9,5 +9,4 @@ tools.pacaur(packages)
 tools.mkdir('~/.vim/bundle')
 with tools.cd('~/.vim/bundle'):
     tools.git_clone('https://github.com/VundleVim/Vundle.vim.git', 'Vundle.vim')
-    tools.bash_cmd(['vim', '+PluginInstall', '+qall'])
 tools.link_conf('~/Projects/dotfiles/confs/vimrc_conf', '~/.vimrc')
