433fa4d4e03e32ac7c959d6bc12a616b6f484f21
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-12 13:24:30 +0200

    forgot choices.py in previous commit

--- scripts/choices.py
+++ scripts/choices.py
@@ -1,3 +1,12 @@
 INSTALL_ENV = 'virtualbox'
 #Real install would be 'machine'
 
+STEPS = ['pacaur.py',
+         'cli_tools.py',
+         'virtualbox.py',
+         'xserver.py',
+         'i3wm.py',
+         'fs_management.py',
+         'fonts.py',
+         'sound.py',
+         'browser.py']
