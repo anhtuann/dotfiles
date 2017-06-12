8501ac4fbee0533fa580dc8642b16eece0e4cb0f
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-12 11:25:19 +0200

    fs_management added

---
+++ scripts/fs_management.py
@@ -0,0 +1,12 @@
+import tools
+
+#INSTALLATION
+packages = ['libmtp',
+            'android-file-transfer',
+            'udisks2',
+            'udiskie',
+            'ntfs-3g',
+            'dosfstools',
+            'hfsprogs',
+            'exfat-utils']
+tools.pacaur(packages)
