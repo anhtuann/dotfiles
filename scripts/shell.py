49eb11d16bb0dbb4c482b06c3ed3649adca8a098
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-12 15:05:34 +0200

    possible solution for shell script

--- scripts/shell.py
+++ scripts/shell.py
@@ -7,9 +7,12 @@ tools.pacaur(packages)
 #CONFIGURATION
 tools.bash_cmd(['rm', '~/.zshrc'])
 tools.link_conf('~/Projects/dotfiles/confs/zshrc_conf', '~/.zshrc')
+tools.bash_cmd(['chsh', '-s', '/bin/zsh'])
 with tools.cd('~/.config'):
     tools.git_clone('https://github.com/chriskempson/base16-shell.git')
 command = 'sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"'
 tools.bash_cmd(command, shell=True)
-tools.bash_cmd('source ~/.zshrc', shell=True)
-tools.bash_cmd('base16_3024', shell=True)
+
+#may need to execute those two commands after reboot because of the change of shell
+tools.bash_cmd(['source', '~/.zshrc'])
+tools.bash_cmd(['base16_3024'])
