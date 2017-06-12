4cd937ee06cb7196729a0045a74c0bcd897701e1
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-12 14:49:28 +0200

    symlink from zsh fixed

--- scripts/shell.py
+++ scripts/shell.py
@@ -5,10 +5,11 @@ packages = ['zsh']
 tools.pacaur(packages)
 
 #CONFIGURATION
+tools.bash_cmd(['rm', '~/.zshrc'])
+tools.link_conf('~/Projects/dotfiles/confs/zshrc_conf', '~/.zshrc')
 with tools.cd('~/.config'):
     tools.git_clone('https://github.com/chriskempson/base16-shell.git')
 command = 'sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"'
 tools.bash_cmd(command, shell=True)
 tools.bash_cmd('source ~/.zshrc', shell=True)
 tools.bash_cmd('base16_3024', shell=True)
-tools.link_conf('~/Projects/dotfiles/confs/zshrc_conf', '~/.zshrc')
