eeec24ca8249296742b7b9bf2baaa57856642630
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-12 17:52:25 +0200

    vim PluginInstall fixed

--- scripts/vim.py
+++ scripts/vim.py
@@ -10,4 +10,5 @@ tools.mkdir('~/.vim/bundle')
 with tools.cd('~/.vim/bundle'):
     tools.git_clone('https://github.com/VundleVim/Vundle.vim.git', 'Vundle.vim')
 tools.link_conf('~/Projects/dotfiles/confs/vimrc_conf', '~/.vimrc')
-tools.bash_cmd(['vim', '+PluginInstall', '+qall'])
+command = 'vim -E -S ~/.vimrc +PluginInstall +qall'
+tools.bash_cmd(command, shell=True)
