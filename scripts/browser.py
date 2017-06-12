22c58f7664bc6b6cc9537b30682c751f24db26c8
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-12 12:03:42 +0200

    webbrowser script added

---
+++ scripts/browser.py
@@ -0,0 +1,11 @@
+import tools
+
+#INSTALL
+packages = ['firefox',
+            'gstreamer',
+            'gst-libav', 
+            'gst-plugins-good']
+tools.pacaur(packages)
+
+#CONFIGURATION
+tools.link_conf('~/Projects/dotfiles/machines/dreamland/confs/vimperatorrc_conf', '~/.vimperatorrc')
