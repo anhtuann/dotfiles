4f39b3d873a36617d066cd6c17b3ea50d350faa0
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-13 09:47:35 +0200

    minimal setup added to choices

--- choices.py
+++ choices.py
@@ -1,14 +1,17 @@
 INSTALL_ENV = 'virtualbox'
 #Real install would be 'machine'
 
-STEPS = ['pacaur.py',
-         'cli_tools.py',
-         'virtualbox.py',
-         'xserver.py',
-         'i3wm.py',
-         'fs_management.py',
-         'fonts.py',
-         'sound.py',
-         'browser.py',
-         'vim.py',
-         'shell.py']
+MINIMAL = ['pacaur.py',
+           'cli_tools.py',
+           'virtualbox.py',
+           'xserver.py',
+           'i3wm.py',
+           'fs_management.py',
+           'fonts.py',
+           'sound.py',
+           'browser.py',
+           'vim.py',
+           'shell.py']
+
+STEPS = list()
+STEPS.extend(MINIMAL)
