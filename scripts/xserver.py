e88b831128e605e0fa8554819fa278e1d612dd7c
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-13 11:07:40 +0200

    conf for dual monitor setup added to xserver

--- scripts/xserver.py
+++ scripts/xserver.py
@@ -9,3 +9,5 @@ tools.pacaur(packages)
 
 #CONFIGURATION
 tools.link_conf('~/Projects/dotfiles/confs/xinitrc_conf', '~/.xinitrc')
+tools.link_conf('~/Projects/dotfiles/confs/virtualoutput_conf', '/etc/X11/xorg.conf.d/60-virtualoutput.conf', sudo=True)
+
