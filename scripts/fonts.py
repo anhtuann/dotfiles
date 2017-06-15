a06d14bf6da498bd7ff1eb7455e2428bbb362a06
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-15 18:33:19 +0200

    ttf-mplus added to fonts script to support vietnamese fonts and a few others

--- scripts/fonts.py
+++ scripts/fonts.py
@@ -3,5 +3,6 @@ from scripts import tools
 #INSTALL
 packages = ['ttf-hack',
             'ttf-symbola',
-            'ttf-droid']
+            'ttf-droid',
+            'ttf-mplus']
 tools.pacaur(packages)
