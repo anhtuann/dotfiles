532ff4337c5272676c1e528c6dd0db2155d1c14a
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-06-17 21:26:46 +0200

    vim-airline and vim-syntastic added and tweaked a little bit

--- scripts/vim.py
+++ scripts/vim.py
@@ -1,7 +1,8 @@
 from scripts import tools
 
 #INSTALL
-packages = ['vim']
+packages = ['vim',
+            'flake8']
 tools.pacaur(packages)
 
 
@@ -10,6 +11,7 @@ tools.mkdir('~/.vim/bundle')
 with tools.cd('~/.vim/bundle'):
     tools.git_clone('https://github.com/VundleVim/Vundle.vim.git', 'Vundle.vim')
 tools.link_conf('~/Projects/dotfiles/confs/vimrc_conf', '~/.vimrc')
+tools.link_conf('~/Projects/dotfiles/confs/pep8_conf', '~/.config/pep8')
 command = 'vim -E -S ~/.vimrc +PluginInstall +qall'
 tools.bash_cmd(command, shell=True)
 with tools.cd('~/.vim/bundle/YouCompleteMe'):
