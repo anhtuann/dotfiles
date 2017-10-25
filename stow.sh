56e517083c9a228ccc74b4ac942a74fde77ca346
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-10-25 21:36:16 +0000

    stow updated to remove graphical apps

--- stow.sh
+++ stow.sh
@@ -1,28 +1,9 @@
-sudo stow -v -t / connman
-stow -v -t ~ deluge
-stow -v -t ~ dunst
-stow -v -t ~ fehbg
 stow -v -t ~ flake8
 stow -v -t ~ git
-stow -v -t ~ gtk3
-stow -v -t ~ i3
-stow -v -t ~ i3status
-stow -v -t ~ keepassxc
-sudo stow -v -t / libinput
-sudo stow -v -t / logind
-stow -v -t ~ mimeapps
-stow -v -t ~ mpv
 sudo stow -v -t / nginx
 stow -v -t ~ pacaur
 sudo stow -v -t / pacman
-stow -v -t ~ polybar
-stow -v -t ~ qt5ct
-stow -v -t ~ qutebrowser
 stow -v -t ~ ranger
-stow -v -t ~ redshift
 stow -v -t ~ rtv
 stow -v -t ~ vim
-stow -v -t ~ xinit
-stow -v -t ~ Xresources
-stow -v -t ~ zathura
 stow -v -t ~ zsh
