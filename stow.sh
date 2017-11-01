f9d0c63c9d00ef12f476231ea898b8b15e0d36e5
Anh Tuan Nguyen <anhtuann2017@gmail.com>
2017-11-01 22:33:51 +0000

    weechat added to stow

--- stow.sh
+++ stow.sh
@@ -3,6 +3,8 @@ stow -v -t ~ git
 sudo stow -v -t / nginx
 stow -v -t ~ pacaur
 sudo stow -v -t / pacman
+sudo stow -v -t / php
 stow -v -t ~ tmux
 stow -v -t ~ vim
+stow -v -t ~ weechat
 stow -v -t ~ zsh
